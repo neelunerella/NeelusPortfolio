4234f4f501d65f35cd1d2e77096ee3d46207ed63
neelunerella <n.neelotpala@gmail.com>
2025-03-16 00:16:35 -0500

    got rid of exclamation mark on hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
     return <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Neelu Nerella</h1>
-            <h2 className={styles.description}>I am passionate about developing the future of technology!</h2>
+            <h2 className={styles.description}>I am passionate about developing the future of technology.</h2>
             <a href="mailto:vnpnerella@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
         <img src={heroImg} alt="Hero Image" className={styles.heroImg}/>
