5ea723d46e38d219d69f0572109225d0d2f045fe
neelunerella <n.neelotpala@gmail.com>
2025-03-18 02:55:58 -0500

    put contact in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
 import { Projects } from "./components/Projects/Projects";
 import { Reading } from "./components/Reading/Reading";
+import { Contact } from "./components/Contact/Contact";
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Projects />
       <Reading />
+      <Contact />
     </div>
   );
 }
